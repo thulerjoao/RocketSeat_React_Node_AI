6ea04422f41d48f58b3b6299b173dca642b28096
thulerjoao <thuler_lima@hotmail.com>
2025-07-07 21:27:40 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { fastifyCors } from "@fastify/cors";
+import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { env } from "./env.ts";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+app.register(fastifyCors, {
+  // origin: "*",
+  origin: "http://localhost:5173",
+});
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get("/health", () => {
+    return "ok"
+})
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log("HTTP Server running!");
+});
